eaea994a0ac6436fba2fdc8a53ddac71cbe3068d
Duncan Graham Walker <walkerdu@bc.edu>
2015-02-02 23:56:23 -0500

    Notes animation and button

--- app/resources/features/new/view.js
+++ app/resources/features/new/view.js
@@ -4,4 +4,21 @@ import Submitting from 'trailblazer/mixins/views/submitting';
 export default Ember.View.extend(
   Submitting, {
 
+  showNotes: Em.computed.bool('controller.notes'),
+
+  actions: {
+    addNotes: function() {
+      this.set('showNotes', true);
+      this.revealNotes();
+    }
+  },
+
+  revealNotes: function() {
+    Em.run.next(this, function() {
+      this.$().find('.notes .input-textarea').velocity('slideDown', {
+        duration: 300
+      });
+    });
+  },
+
 });
