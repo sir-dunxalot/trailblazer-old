805f0efd48f875fdda71cd0101810c551776ac47
Duncan Graham Walker <walkerdu@bc.edu>
2015-02-06 00:57:09 -0500

    Image loading component added

---
+++ app/components/image-tag.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  alt: null,
+  attributeBindings: ['alt', 'src'],
+  classNameBindings: ['loaded:fade-in:fade-out'],
+  classNames: ['image'],
+  loaded: false,
+  src: null,
+  tagName: 'img',
+
+  checkForAlt: function() {
+    Ember.assert('You must pass an alt option to the {{lazy-image}} component', this.get('alt'));
+  }.on('init'),
+
+  lazyLoad: function() {
+    this.$().on('load', Em.run.bind(this, this.set, 'loaded', true));
+  }.on('didInsertElement')
+});
