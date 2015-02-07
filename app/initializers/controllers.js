abaaa21e7286f464e57dafbc67b39bfca23f2f70
Duncan Graham Walker <walkerdu@bc.edu>
2015-02-06 22:10:26 -0500

    Definition list for feature dates. Buggy

--- app/initializers/controllers.js
+++ app/initializers/controllers.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 
 export function initialize(container, app) {
+  var dateFormat = app.dateFormat;
+  var shortDateFormat = dateFormat.split(',')[0];
 
   Ember.ControllerMixin.reopen(
     Ember.Evented, {
 
     userId: Ember.computed.readOnly('session.content.uid'),
-    dateFormat: app.dateFormat
+    dateFormat: dateFormat,
+    shortDateFormat: shortDateFormat
 
   });
 
--- app/resources/feature/index/controller.js
+++ app/resources/feature/index/controller.js
@@ -2,4 +2,32 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   completedTasks: Ember.computed.filter('tasks', 'completed', true),
+  lowerDate: null,
+  upperDate: null,
+
+  setDates: function() {
+    var _this = this;
+
+    _this.get('stages').then(function(stages) {
+      stages.forEach(function(stage) {
+        stage,get('type').then(function(type) {
+          var name = type.get('name');
+          var duration = stage.get('duration');
+
+          if (name === 'research') {
+            var startDate = moment(_this.get('startDate'));
+            var lowerDate = startDate.add(duration, 'd');
+
+            this.set('lowerDate', lowerDate);
+          } else if (name === 'testing') {
+            var endDate = moment(_this.get('endDate'));
+            var upperDate = endDate.subtract(duration, 'd');
+
+            this.set('upperDate', lowerDate);
+          }
+        });
+      });
+    });
+  }.observes('stages.@each.duration')
+
 });
