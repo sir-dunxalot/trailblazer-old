3c880406a40c1c2eb5df7a112708bead59ab6da3
Duncan Graham Walker <walkerdu@bc.edu>
2015-08-01 22:30:09 -0400

    Backlog ranking and saving working

--- app/resources/backlog/controller.js
+++ app/resources/backlog/controller.js
@@ -4,4 +4,15 @@ const { computed } = Ember;
 
 export default Ember.Controller.extend({
   backlogFeatures: computed.filterBy('model', 'inBacklog'),
+  rankedBacklogFeatures: computed.sort('backlogFeatures', 'sortBy'),
+  sortBy: ['backlogPosition'],
+
+  actions: {
+    reorderBacklog(backlog) {
+      backlog.forEach(function(feature, i) {
+        feature.set('backlogPosition', i);
+        feature.save();
+      });
+    }
+  }
 });
