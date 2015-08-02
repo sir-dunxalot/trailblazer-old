5c514ae67202e5ad11b175ee674dada869cc6666
Duncan Graham Walker <walkerdu@bc.edu>
2015-08-02 18:12:53 -0400

    query params for inBacklog workign on feature edit route

--- app/resources/feature/edit/controller.js
+++ app/resources/feature/edit/controller.js
@@ -1,6 +1,7 @@
 import FeatureController from 'trailblazer/resources/features/new/controller';
 
 export default FeatureController.extend({
+  saveButtonText: 'save',
 
   cancel() {
     this.transitionToRoute('feature', this.get('model'));
--- app/resources/feature/edit/route.js
+++ app/resources/feature/edit/route.js
@@ -4,6 +4,8 @@ import Ember from 'ember';
 export default Ember.Route.extend(
   DirtyRecordHandler, {
 
+  _passedQueryParams: null,
+
   /* TODO - is there a way to rollback nested resources? */
 
   undoDurationChanges: Ember.on('willTransition', function() {
@@ -16,8 +18,28 @@ export default Ember.Route.extend(
     });
   }),
 
-  model: function() {
+  model: function(params) {
+    this.set('_passedQueryParams', params); // Hack for setupController
+
     return this.modelFor('feature');
   },
+
+  setupController(controller, model) {
+    const inBacklogParam = this.get('_passedQueryParams.inBacklog');
+
+    let inBacklog;
+
+    if (inBacklogParam === 'true') {
+      inBacklog = true;
+    } else if (inBacklogParam === 'false') {
+      inBacklog = false;
+    }
+
+    if (typeof inBacklog === 'boolean') {
+      model.set('inBacklog', inBacklog);
+    }
+
+    this._super(controller, model);
+  }
 
 });
